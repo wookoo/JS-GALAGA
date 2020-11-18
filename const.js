78b983b9ef1954589536234ae22047d8ed625b90
wookoo <nanayagoon@daum.net>
2020-11-18 21:54:16 +0900

    상수 정보

---
+++ const.js
@@ -0,0 +1,17 @@
+
+const KEY = {
+  ESC: 27,
+  SPACE: 32,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  P: 80,
+  Q: 81
+};
+const ITEM_TYPE = {
+  SPEED_UP : 1,
+  LIFE_UP : 2,
+  SHOOT_SPEED_UP : 3,
+  SHOOT_DELAY_REDUCE : 4
+};
