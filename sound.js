42e3b17733508cb71cbc204f0caf59a58ac8b0f1
wookoo <nanayagoon@daum.net>
2020-11-19 10:25:27 +0900

    사운드 스크립트 추가

---
+++ sound.js
@@ -0,0 +1,66 @@
+class Sound {
+    constructor(parent){
+        this.parent = parent;
+        this.sounds = [];
+        this.muted = true;
+    }
+
+    create(src, id, loop = false){
+        let audio = document.createElement("audio");
+        audio.src = src;
+        audio.id = id;
+        audio.muted = true;
+        this.sounds.push(audio);
+        this.parent.append(audio);
+
+        if(loop){
+            audio.setAttribute("loop", "")
+        }
+
+        return audio;
+    }
+
+}
+
+Sound.prototype.soundSetting = function(){
+    let soundItems = document.querySelectorAll(".sound-item");
+    for(let soundItem of soundItems){
+        soundItem.addEventListener("click", (e)=>{
+            this.muteToggle();
+        });
+    }
+};
+
+Sound.prototype.muteToggle = function(){
+    if(!this.muted){
+        for(let sound of this.sounds){
+            sound.muted = true;
+        }
+        document.querySelector("#sound-speaker").innerHTML = "\u{1F507}";
+        document.querySelector("#sound-description").innerHTML = "off";
+        this.muted = true;
+    }else{
+      for(let sound of this.sounds){
+          sound.muted = false;
+      }
+      this.muted = false;
+    }
+};
+
+Sound.prototype.pause = function(){
+    for(let sound of this.sounds){
+        sound.pause();
+    }
+}
+
+Sound.prototype.play = function(){
+    for(let sound of this.sounds){
+        sound.play();
+    }
+}
+
+let sound = new Sound(document.querySelector("#sound")),
+    shootSound = sound.create("sound/shoot.mp3", "shoot_sound");
+    itemSound = sound.create("sound/getItem.mp3", "item_sound");;
+sound.muteToggle();
+sound.soundSetting();
