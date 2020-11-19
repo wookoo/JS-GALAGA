d3d20eacacc3535b015d6e3f64d8f11748bb7df6
wookoo <nanayagoon@daum.net>
2020-11-19 19:38:58 +0900

    이미지 관련 상수 추가

--- const.js
+++ const.js
@@ -26,3 +26,35 @@ const COLOR =[
 
 ]
 const ITEM_WIDTH = 20;
+const HEART_IMG = new Image();
+HEART_IMG.src = "image/heart.png";
+const MOVE_SPEED_IMG = new Image();
+MOVE_SPEED_IMG.src = "image/move.png";
+const SHOOT_DELAY_IMG = new Image();
+SHOOT_DELAY_IMG.src = "image/shoot-delay.png";
+const BULLET_SPEED_IMG = new Image();
+BULLET_SPEED_IMG.src = "image/bullet-speed.png";
+const MAX_ENEMY_SIZE = 30;
+const DEFAULT_ENEMY_SIZE = 20;
+
+const ITEM_IMAGES =[
+  MOVE_SPEED_IMG,
+  BULLET_SPEED_IMG,
+  SHOOT_DELAY_IMG,
+  HEART_IMG
+];
+
+const ENEMY_IMG_1 = new Image();
+ENEMY_IMG_1.src = "image/enemy1.png"
+const ENEMY_IMG_2 = new Image();
+ENEMY_IMG_2.src = "image/enemy2.png"
+const ENEMY_IMG_3 = new Image();
+ENEMY_IMG_3.src = "image/enemy3.png"
+const ENEMY_IMG_4 = new Image();
+ENEMY_IMG_4.src = "image/enemy4.png"
+const ENEMY_IMAGES = [
+  ENEMY_IMG_1,
+  ENEMY_IMG_2,
+  ENEMY_IMG_3,
+  ENEMY_IMG_4
+];
